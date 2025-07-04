d0857409e88805fa50553b790c1e37f75b870129
Hamzeh Azari Hashjin <99587451+hamzeh-azari@users.noreply.github.com>
2025-07-04 13:56:04 -0400

    Create Copy_AD_permissions_from_an_old_user_to_a_new_user.ps1

---
+++ Copy_AD_permissions_from_an_old_user_to_a_new_user.ps1
@@ -0,0 +1,9 @@
+# If you're a system administrator or technical support analyst, you might need to replicate all permissions from an existing user to a newly created one. With the simple PowerShell script below, you can easily achieve this within Active Directory.
+Note: Make sure to run PowerShell as Administrator (elevated mode).
+
+$oldUser = "OldUserName"
+$newUser = "NewUserName"
+$groups = Get-ADUser $oldUser -Property MemberOf | Select-Object -ExpandProperty MemberOf
+foreach ($group in $groups) {
+    Add-ADGroupMember -Identity $group -Members $newUser
+}
